ad2af22adae639802bce01a70f7ab723fc526f23
squarif <aliadnanarif25@gmail.com>
2021-03-24 01:10:38 +0500

    Notifications

--- navigation/Navigator.js
+++ navigation/Navigator.js
@@ -1,297 +1,318 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "react-navigation-drawer";
-import React from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-import Icon from "react-native-vector-icons/Ionicons";
-import CreateTasks from "../screens/Tasks/CreateTasks";
-import EditTask from "../screens/Tasks/EditTask";
-import Tasks from "../screens/Tasks/Tasks";
-import Collaborations from "../screens/Home/Collaborations";
-import Settings from "../screens/Home/Settings";
-import Signin from "../screens/Home/Signin";
-import Signup from "../screens/Home/Signup";
-import Chat from "../screens/Chat/Chat";
-import Chatbox from "../screens/Chat/Chatbox";
-import Calendar from "../screens/Calendar/Calendar";
-import CreateEvent from "../screens/Calendar/CreateEvent";
-import EditEvent from "../screens/Calendar/EditEvent";
-import Notes from "../screens/Notes/Notes";
-import EditNote from "../screens/Notes/EditNote";
-import Notification from "../screens/Notification/Notification";
+import {createAppContainer} from 'react-navigation'
+import {createStackNavigator} from 'react-navigation-stack'
+import {createDrawerNavigator} from 'react-navigation-drawer'
+import React from 'react'
+import {Platform, StyleSheet, Text, View} from 'react-native'
+import Icon from 'react-native-vector-icons/Ionicons'
+import CreateTasks from '../screens/Tasks/CreateTasks'
+import EditTask from '../screens/Tasks/EditTask'
+import Tasks from '../screens/Tasks/Tasks'
+import Collaborations from '../screens/Home/Collaborations'
+import Settings from '../screens/Home/Settings'
+import Signin from '../screens/Home/Signin'
+import Signup from '../screens/Home/Signup'
+import Chat from '../screens/Chat/Chat'
+import Chatbox from '../screens/Chat/Chatbox'
+import Calendar from '../screens/Calendar/Calendar'
+import CreateEvent from '../screens/Calendar/CreateEvent'
+import EditEvent from '../screens/Calendar/EditEvent'
+import Notes from '../screens/Notes/Notes'
+import EditNote from '../screens/Notes/EditNote'
+import Notification from '../screens/Notification/Notification'
 
 import Members from '../screens/Members/Members'
 
-const TaskNavigator = createStackNavigator(
-    {
-        // Collaborations: Collaborations,
-        // Hamburger: Hamburger,
-        // Settings: Settings,
-        // Signin: Signin,
-        // Signup: Signup,
-        // Chat: Chat,
-        // Chatbox: Chatbox,
-        // Calendar: Calendar,
-        // CreateEvent: CreateEvent,
-        // EditEvent: EditEvent,
-        // Notes: Notes,
-        // EditNote: EditNote,
-        // Notification: Notification,
-        //Members: Members,
-        // Tasks: Tasks,
-        // CreateTasks: CreateTasks,
-        // EditTask: EditTask,
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "black",
-      },
-      headerTintColor: "white",
-    },
-  }
-);
+const TaskNavigator = createStackNavigator({
+    // Collaborations: Collaborations,
+    // Hamburger: Hamburger,
+    // Settings: Settings,
+    // Signin: Signin,
+    // Signup: Signup,
+    // Chat: Chat,
+    // Chatbox: Chatbox,
+    // Calendar: Calendar,
+    // CreateEvent: CreateEvent,
+    // EditEvent: EditEvent,
+    // Notes: Notes,
+    // EditNote: EditNote,
+    Notification: Notification,
+    // Members: Members,
+    // Tasks: Tasks,
+    // CreateTasks: CreateTasks,
+    // EditTask: EditTask,
+})
 
 const CollaborationNavigator = createStackNavigator(
-  {
-    Signup: Signup,
-    Collaborations: Collaborations,
-    Signin: Signin,
-    Settings: Settings,
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "black",
-      },
-      headerTintColor: "white",
+    {
+        Signup: Signup,
+        Collaborations: Collaborations,
+        Signin: Signin,
+        Settings: Settings,
     },
-  }
-);
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerTintColor: 'white',
+        },
+    }
+)
 
 const NotesNavigator = createStackNavigator(
-  {
-    Notes: Notes,
-    EditNote: EditNote,
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "black",
-      },
-      headerTintColor: "white",
+    {
+        Notes: Notes,
+        EditNote: EditNote,
     },
-  }
-);
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerTintColor: 'white',
+        },
+    }
+)
 
 const CalendarNavigator = createStackNavigator(
-  {
-    Calendar: Calendar,
-    CreateEvent: CreateEvent,
-    EditEvent: EditEvent,
-    // Notification: Notification,
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "black",
-      },
-      headerTintColor: "white",
+    {
+        Calendar: Calendar,
+        CreateEvent: CreateEvent,
+        EditEvent: EditEvent,
+        // Notification: Notification,
     },
-  }
-);
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerTintColor: 'white',
+        },
+    }
+)
 
 const ChatNavigator = createStackNavigator(
-  {
-    Chat: Chat,
-    Chatbox: Chatbox,
-  },
-  {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: "black",
-      },
-      headerTintColor: "white",
+    {
+        Chat: Chat,
+        Chatbox: Chatbox,
     },
-  }
-);
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerTintColor: 'white',
+        },
+    }
+)
+
+const NotificationsNavigator = createStackNavigator(
+    {
+        Notification: Notification,
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerTintColor: 'white',
+        },
+    }
+)
+
+const MembersNavigator = createStackNavigator(
+    {
+        Members: Members,
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: 'black',
+            },
+            headerTintColor: 'white',
+        },
+    }
+)
 
 CreateEvent.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Create Event",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Create Event',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 EditEvent.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Edit Event",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Edit Event',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 Calendar.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Calendar",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Calendar',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 Tasks.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Tasks",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Tasks',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 CreateTasks.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Create Tasks",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Create Tasks',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 EditTask.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Edit Task",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Edit Task',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 Chat.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Chat",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Chat',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 Chatbox.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Chat",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Chat',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 Notes.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Notes",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Notes',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 EditNote.navigationOptions = (navData) => {
-  return {
-    headerTitle: "Edit Note",
-    headerLeft: () => (
-      <Icon
-        type="ionicon"
-        name="md-menu"
-        color="white"
-        size={35}
-        onPress={() => {
-          navData.navigation.toggleDrawer();
-        }}
-      />
-    ),
-  };
-};
+    return {
+        headerTitle: 'Edit Note',
+        headerLeft: () => (
+            <Icon
+                type="ionicon"
+                name="md-menu"
+                color="white"
+                size={35}
+                onPress={() => {
+                    navData.navigation.toggleDrawer()
+                }}
+            />
+        ),
+    }
+}
 
 const MainNavigator = createDrawerNavigator({
-  Notes: NotesNavigator,
-  Tasks: TaskNavigator,
-  Calendar: CalendarNavigator,
-  Chat: ChatNavigator,
-  Settings: Settings,
-});
+    Notes: NotesNavigator,
+    Tasks: TaskNavigator,
+    Calendar: CalendarNavigator,
+    Chat: ChatNavigator,
+    Notification: NotificationsNavigator,
 
-export default createAppContainer(MainNavigator);
+    Settings: Settings,
+    Members: MembersNavigator,
+})
+
+export default createAppContainer(MainNavigator)
--- screens/Notes/Notes.js
+++ screens/Notes/Notes.js
@@ -1,29 +1,22 @@
-import React from "react";
-import {
-  View,
-  Text,
-  Image,
-  Button,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
-import NoteTile from "../../components/NoteTile";
+import React from 'react'
+import {View, Text, Image, Button, StyleSheet, ScrollView} from 'react-native'
+import NoteTile from '../../components/NoteTile'
 
 const Notes = () => {
-  return (
-    <View style={styles.container}>
-      <Text>The Notes Screen!</Text>
-    </View>
-  );
-};
+    return (
+        <View style={styles.container}>
+            <Text>The Notes Screen!</Text>
+        </View>
+    )
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
 
-export default Notes;
+export default Notes
